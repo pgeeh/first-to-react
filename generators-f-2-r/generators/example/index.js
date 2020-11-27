a64e3759df98e22cdad23c1cd5dede9c66885713
Peter Hilton <pgeeh@users.noreply.github.com>
2020-11-27 13:44:47 -0500

    Add generator for examples

---
+++ generators-f-2-r/generators/example/index.js
@@ -0,0 +1,63 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the superior ${chalk.red('generator-f-2-r')} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'includeCode',
+        message: 'Would you like to include a code file?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'fullName',
+        message: 'Name of the example?',
+        default: 'NewExample'
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      const cleanName = props.fullName; // Eventually clean this
+      this.props = {
+        ...props,
+        cleanName,
+      };
+    });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.js'),
+      this.destinationPath(`${this.props.cleanName}/index.js`), 
+      this.props,
+    );
+    
+    this.fs.copyTpl(
+      this.templatePath('Template.md'),
+      this.destinationPath(`${this.props.cleanName}/${this.props.cleanName}.md`), 
+      this.props,
+    );
+    
+    if (this.props.includeCode) {
+      this.fs.copyTpl(
+        this.templatePath('Template.jsexample'),
+        this.destinationPath(`${this.props.cleanName}/${this.props.cleanName}.jsexample`), 
+        this.props,
+      );
+    }
+  }
+
+  install() {
+    // this.installDependencies();
+  }
+};
---
+++ generators-f-2-r/generators/example/templates/index.js
@@ -0,0 +1,11 @@
+
+import info from './<%= cleanName %>.md';<%if (includeCode === true) { %>
+import code from './<%= cleanName %>.jsexample';<% } %>
+
+const config = {
+  info,
+  name: '<%= fullName %>',<%if (includeCode === true) { %>
+  code,<% } %>
+};
+
+export default config;
