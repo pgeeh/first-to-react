dc6ab271e94dfe862e553ad74c2342400e7f59ba
Peter Hilton <pgeeh@users.noreply.github.com>
2020-12-08 19:26:28 -0500

    Update generator to take a number of examples

--- generator-f-2-r/generators/example/templates/index.js
+++ generator-f-2-r/generators/example/templates/index.js
@@ -1,11 +1,16 @@
 
-import info from './<%= cleanName %>.md';<%if (includeCode === true) { %>
-import code from './<%= cleanName %>.jsexample';<% } %>
+import info from './<%= cleanName %>.md';<% for(let i = 1; i <= codeCount; i++) { %>
+import code<%= i %> from './<%= cleanName %><%= i %>.jsexample';<% } %>
 
 const config = {
   info,
-  name: '<%= fullName %>',<%if (includeCode === true) { %>
-  code,<% } %>
+  name: '<%= fullName %>',<%if (codeCount > 0) { %>
+  code: [
+    <% for(let i = 1; i <= codeCount; i++) { %>{
+      name: 'Example<%= i %>',
+      code: code<%= i %>,
+    },<% } %>
+  ],<% } %>
   children: [
   ],
 };
